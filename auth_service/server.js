b83396613705aedb7b7fd432e21fae600ba2bbb8
Dirk Breuer <1948+code-later@users.noreply.github.com>
2022-06-16 11:50:37 +0200

    GH-4: Create super simple welcome server (#5)

---
+++ auth_service/server.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const express = require('express');
+const Redis = require("ioredis");
+const cors = require('cors');
+const path = require('path');
+
+// Connect to Redis with REDIS_URL from ENV
+const redis = new Redis(process.env.REDIS_URL);
+
+// Constants
+const PORT = 8080;
+const HOST = process.env.BINDING;
+
+// App
+var app = module.exports = express();
+
+// Enable CORS for localhost origins
+app.use(cors({ origin: /localhost/, credentials: true }));
+
+// Read form data
+app.use(express.urlencoded({ extended: false }));
+
+// Config
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.redirect('/auth/signup');
+});
+
+app.get('/signup', function(req, res){
+  res.render('signup');
+});
+
+app.post('/signup', function(req, res){
+  const email = req.body.email;
+  const password = req.body.password;
+
+  redis.set(email, password); // NEVER store plain text passwords!
+
+  res.redirect('/auth/login');
+});
+
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
+app.post('/login', function(req, res){
+  const email = req.body.email;
+  const password = req.body.password;
+
+  redis.get(email, (err, result) => {
+    if (err) {
+      res.render('failed');
+    } else {
+
+      if (result === password) {
+        res.render('welcome', { email: email });
+      } else {
+        res.render('failed');
+      }
+    }
+  });
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
