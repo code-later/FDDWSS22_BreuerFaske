76de95ce986719c30f0584c75ba962e688926301
Dirk Breuer <mail@breuer.io>
2022-06-16 17:30:57 +0200

    GH-6: Setup Rails to work in sub-directory

--- game_service/config/application.rb
+++ game_service/config/application.rb
@@ -31,6 +31,8 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.relative_url_root = "/game"
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
